ee43ac8c24712b2a37bfc36f487f4d22c682e496
Harenabs21 <hei.harena.6@gmail.com>
2025-05-15 22:00:13 +0300

    add ignores property in linter config file and delete .eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,19 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    ignores: [
+      "node_modules",
+      "dist",
+      "build",
+    ],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
 ]);
