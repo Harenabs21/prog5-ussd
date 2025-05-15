dca3767082c495d9c4232e06924e06f4995ae84a
Harenabs21 <hei.harena.6@gmail.com>
2025-05-15 21:31:40 +0300

    create the display menu

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log('Welcome to the Menu')
+import { displayMenu } from './menu'
+import { handleChoice } from'./menu'
+import  readline  from 'readline';
+import { mainMenuOptions } from './constants'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function mainMenu(options: Record<string, string>): void {
+    displayMenu(options);
+    rl.question("Choisissez une option : ", (choice: string) => {
+        handleChoice(choice, options);
+        if (choice === "9") {
+            console.log("Merci d'avoir utilisé notre application !");
+            rl.close();
+            return;
+        }
+        mainMenu(options);
+    });
+}
+
+mainMenu(mainMenuOptions)
